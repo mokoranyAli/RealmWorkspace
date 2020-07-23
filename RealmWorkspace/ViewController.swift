4f2f69d067940419eb991d5f31207865b64ed361
Mohamed <mohammed.korany.ali@gmail.com>
2020-07-23 13:44:45 +0200

    Initial Commit

---
+++ RealmWorkspace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmWorkspace
+//
+//  Created by Mohamed Korany Ali on 7/23/20.
+//  Copyright © 2020 Mohamed Korany Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
